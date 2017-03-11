4ab363f53ee983232222ba213241f32522cc2523
Vaughan Kitchen <v.kitchen@gnoms.org>
2017-03-11 22:57:32 +1300

    option type

--- example.c
+++ example.c
@@ -1,7 +1,70 @@
+#include <stdio.h>
 #include "option.h"
 
-DEFINE_OPTION_TYPE(option_int, int);
+OPTION_TYPEDEF(int, option_int);
+
+struct cat
+	{
+	int legs;
+	};
+
+OPTION_TYPEDEF(struct cat, option_cat);
+OPTION_TYPEDEF(struct cat *, option_pointer_cat);
 
 int main()
 	{
+	option_int a[5];
+	OPTION_NONE(&a[0]);
+	OPTION_NONE(&a[1]);
+	OPTION_SOME(&a[2], 5);
+	OPTION_SOME(&a[3], -52);
+	OPTION_SOME(&a[4], 12);
+
+	for (int i = 0; i < 5; i++)
+		{
+		switch (a[i].type)
+			{
+			case OPTION_NONE:
+				printf("a[%d] is storing nothing\n", i);
+				break;
+			case OPTION_SOME:
+				printf("a[%d] is storing %d\n", i, a[i].value);
+				break;
+			}
+		}
+
+	struct cat c = {5};
+	option_cat oc;
+	OPTION_SOME(&oc, c);
+	switch (oc.type)
+		{
+		case OPTION_NONE:
+			printf("I have no cat\n");
+			break;
+		case OPTION_SOME:
+			printf("I have a cat with %d legs\n", oc.value.legs);
+			break;
+		}
+
+	option_pointer_cat opc;
+	OPTION_SOME(&opc, &c);
+	switch (opc.type)
+		{
+		case OPTION_NONE:
+			printf("I have no pointy cat\n");
+			break;
+		case OPTION_SOME:
+			printf("I have a pointy cat with %d legs\n", opc.value->legs);
+			break;
+		}
+	OPTION_NONE(&opc);
+	switch (opc.type)
+		{
+		case OPTION_NONE:
+			printf("I have no pointy cat\n");
+			break;
+		case OPTION_SOME:
+			printf("I have a pointy cat with %d legs\n", opc.value->legs);
+			break;
+		}
 	}
--- option.h
+++ option.h
@@ -4,10 +4,19 @@
 /* Values can be Some or None */
 enum option_type {OPTION_NONE, OPTION_SOME};
 
-#define DEFINE_OPTION_TYPE(content_type_name__, content_type__)	\
+#define OPTION_TYPEDEF(content_type__, content_type_name__)		\
 typedef struct {												\
 	enum option_type type;										\
 	content_type__ value;										\
 } content_type_name__
+
+#define OPTION_NONE(a) do {										\
+	(a)->type = OPTION_NONE;									\
+} while(0)
+
+#define OPTION_SOME(a, val)	do {								\
+	(a)->type = OPTION_SOME;									\
+	(a)->value = (val);											\
+} while(0)
 
 #endif
